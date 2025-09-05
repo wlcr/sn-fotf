601fd7a7ae5568dcafd3d6507eb3a0bc7bca15f7
Hugh Donkin <49598076+hsdonkin@users.noreply.github.com>
2025-09-04 20:04:30 -0700

    Delete index.ts

--- app/components/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all components from their respective folders
-export {Header} from './Header/Header';
-export type {HeaderProps} from './Header/Header';
-
-export {Footer} from './Footer';
-export type {FooterProps} from './Footer';
