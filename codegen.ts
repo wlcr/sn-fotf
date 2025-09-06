b0ff08fcb7143ec06b35c6df5754ad0ffe8e09f8
Hugh Donkin <49598076+hsdonkin@users.noreply.github.com>
2025-09-06 11:03:27 -0700

    fix issue where codegen wouldn't find graphql queries outside of routes directory lol

---
+++ codegen.ts
@@ -0,0 +1,19 @@
+import type {CodegenConfig} from '@graphql-codegen/cli';
+import {pluckConfig, preset, getSchema} from '@shopify/hydrogen-codegen';
+
+export default {
+  overwrite: true,
+  pluckConfig,
+  generates: {
+    'storefrontapi.generated.d.ts': {
+      schema: getSchema('storefront'),
+      documents: ['./*.{ts,tsx,js,jsx}', './app/**/*.{ts,tsx,js,jsx}'],
+      preset,
+    },
+    'customeraccountapi.generated.d.ts': {
+      schema: getSchema('customer-account'),
+      documents: ['./app/graphql/customer-account/*.{ts,tsx,js,jsx}'],
+      preset,
+    },
+  },
+} as CodegenConfig;
