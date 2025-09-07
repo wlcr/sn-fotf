b0ca56130b7cba68d8c942bd47571179f3b2826d
Hugh Donkin <49598076+hsdonkin@users.noreply.github.com>
2025-09-06 17:23:31 -0700

    tweak column sizes

--- app/components/ProductDetail/ProductDetail.tsx
+++ app/components/ProductDetail/ProductDetail.tsx
@@ -43,11 +43,16 @@ export default function ProductDetail({product}: ProductDetailProps) {
   });
 
   return (
-    <Container px={{initial: '4', sm: '6'}} py={{initial: '6', sm: '9'}}>
-      <Grid columns={{initial: '1', sm: '2'}} gap="8" align="start">
+    <Container px={{initial: '5', sm: '6'}} py={{initial: '6', sm: '9'}}>
+      <Grid columns={{initial: '1', sm: '43% 1fr'}} gap="5" align="start">
         <ProductMediaGallery media={product.media} />
 
-        <Grid align="start" gap="5">
+        <Grid
+          align="start"
+          gap="5"
+          px={{initial: '0', sm: '6'}}
+          py={{initial: '0', sm: '5'}}
+        >
           <Grid gap="3">
             <div className="heading-4">Chico, Lorem Ipsum</div>
             <h1 className="heading-1">{product.title}</h1>
