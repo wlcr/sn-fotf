50a9a368d3d0b9d28560cde91896b4ebce468420
Hugh Donkin <49598076+hsdonkin@users.noreply.github.com>
2025-09-10 16:37:59 -0700

    replace barrel imports with arrays

--- studio/schemaTypes/objects/index.ts
+++ studio/schemaTypes/objects/index.ts
@@ -1,17 +1,38 @@
-export {blockContent} from './blockContent';
-export {callToAction} from './callToAction';
-export {collectionBlock} from './collectionBlock';
-export {link} from './link';
-export {mediaImage} from './mediaImage';
-export {imageContentBlock} from './imageContentBlock';
-export {linkButton} from './linkButton';
-export {contentBlock} from './contentBlock';
-export {imageBlock} from './imageBlock';
-export {menu} from './menu';
-export {faqBlock} from './faqBlock';
-export {ctaBlock} from './ctaBlock';
-export {specialComponentBlock} from './specialComponentBlock';
-export {newsletterBlock} from './newsletterBlock';
-export {mediaVimeo} from './mediaVimeo';
-export {pageSection} from './pageSection';
-export {openGraph, globalOpenGraph} from './openGraph';
+import {blockContent} from './blockContent';
+import {callToAction} from './callToAction';
+import {collectionBlock} from './collectionBlock';
+import {link} from './link';
+import {mediaImage} from './mediaImage';
+import {imageContentBlock} from './imageContentBlock';
+import {linkButton} from './linkButton';
+import {contentBlock} from './contentBlock';
+import {imageBlock} from './imageBlock';
+import {menu} from './menu';
+import {faqBlock} from './faqBlock';
+import {ctaBlock} from './ctaBlock';
+import {specialComponentBlock} from './specialComponentBlock';
+import {newsletterBlock} from './newsletterBlock';
+import {mediaVimeo} from './mediaVimeo';
+import {pageSection} from './pageSection';
+import {openGraph, globalOpenGraph} from './openGraph';
+
+export default [
+  blockContent,
+  callToAction,
+  collectionBlock,
+  link,
+  mediaImage,
+  imageContentBlock,
+  linkButton,
+  contentBlock,
+  imageBlock,
+  menu,
+  faqBlock,
+  ctaBlock,
+  specialComponentBlock,
+  newsletterBlock,
+  mediaVimeo,
+  pageSection,
+  openGraph,
+  globalOpenGraph,
+];
